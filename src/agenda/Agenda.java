4ab65b6795d1efa10fbf70031c56ef4930832f1f
larisarapio <larissa.sarapio@gmail.com>
2025-01-21 09:08:36 -0300

    feat:funcoes agenda para contatos

--- src/agenda/Agenda.java
+++ src/agenda/Agenda.java
@@ -1,5 +1,37 @@
 package agenda;
 
-class Agenda {
-    //Adicionar Remover Buscar Editar Exibir - funções para contatos
+import java.util.ArrayList;
+import java.util.List;
+
+class Agenda extends Usuario{
+    private List<Agenda> contatos;
+
+    public Agenda(String nome, String telefone, String email, Categoria categoria, String notas) {
+        super(nome, telefone, email, categoria, notas);
+    }
+
+    public void buscaContato() {
+        contatos = new ArrayList<Agenda>();
+
+    }
+
+    public void adicinarContato(Agenda contato) {
+        contatos.add(contato);
+
+    }
+
+    public void removerContato(Agenda contato){
+        contatos.remove(contato);
+
+    }
+
+    public void editarContato(Agenda contato){
+        contatos.set(contatos.indexOf(contato), contato);
+
+    }
+
+    public List<Agenda> listarContatos() {
+        return contatos;
+
+    }
 }
