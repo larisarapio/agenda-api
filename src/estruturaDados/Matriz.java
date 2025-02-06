526b254c30249a4cf6104230e499169aae318173
larisarapio <larissa.sarapio@gmail.com>
2025-02-06 08:45:13 -0300

    edit: mais estudos sobre estrutura de dados

--- src/estruturaDados/Matriz.java
+++ src/estruturaDados/Matriz.java
@@ -1,5 +1,6 @@
 package estruturaDados;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -31,14 +32,21 @@ public class Matriz {
         frutas.stream()
                 .map(name -> name.toUpperCase())
                 .forEach(System.out::println);
+           */
 
-        List<String> fruta1 = Arrays.asList("3maça", "2banana", "9uva");
+        //aqui ele coloca na ordem por numeros e por letras
+        List<String> fruta1 = Arrays.asList("maça", "banana", "uva");
 
         fruta1.stream()
                 .sorted()
                 .forEach(System.out::println);
 
-        */
+
+
+        //em colletions não faz isso, faz apenas cm numeros - integer
+        List<String> fruta2 = Arrays.asList("20maça", "2banana", "9uva");
+        Collections.sort(fruta2);
+        System.out.println(fruta2);
 
         // soma
         int [] soma = {1, 2, 3, 4};
@@ -51,6 +59,11 @@ public class Matriz {
         int menor = Arrays.stream(menorMaiorNumero).min().getAsInt();
         System.out.println(maior + " maior e maior " + menor);
 
+        //ou usar collections - aqui parece ser mais otimizado
+        List<Integer> menorMaiorNumero1 = Arrays.asList(5, 1, 8, 3);
+        System.out.println(Collections.min(menorMaiorNumero1)); // 1
+        System.out.println(Collections.max(menorMaiorNumero1)); // 8
+
         //inverte
         Integer[] numeros = {1, 2, 3, 4, 5};
         List<Integer> listaInvertida = Arrays.stream(numeros)
@@ -59,6 +72,34 @@ public class Matriz {
         Collections.reverse(listaInvertida);
 
         System.out.println("invertindo:" + listaInvertida);
+
+
+        //outra forma - daria para fazer com strings tb
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        Collections.reverse(numbers);
+        System.out.println("outra forma de reverter: " + numbers);
+
+        // busca
+        /*
+
+        A ordenação padrão no Java diferencia maiúsculas de minúsculas:
+
+        "Verde" (com V maiúsculo) vem antes de "azul" e "rosa".
+        "verde" (com v minúsculo) vem depois de todas as strings com inicial maiúscula.
+        Para ignorar maiúsculas/minúsculas, você pode usar um comparador personalizado:
+            Collections.sort(cores, String.CASE_INSENSITIVE_ORDER);
+         */
+        List<String> cores = Arrays.asList("rosa", "azul", "verde");
+        int posicao = Collections.binarySearch(cores, "verde");
+        System.out.println(posicao);
+
+        //Imutabilidade de coleções
+        // a "lista" permite modificações
+        List<String> lista = new ArrayList<>(Arrays.asList("A", "B", "C"));
+        // não permite nenhuma modificação.
+        List<String> listaImutavel = Collections.unmodifiableList(lista);
+
+        System.out.println(listaImutavel);
 
     }
 }
